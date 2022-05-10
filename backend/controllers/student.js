9bbe2f3618a11f5837e52c0ac0e134208118d326
EJK0 <ephraimkim1@gmail.com>
2022-05-10 19:46:22 -0400

    cleaned up student.js

--- backend/controllers/student.js
+++ backend/controllers/student.js
@@ -58,33 +58,6 @@ module.exports.setAppType = async (req, res) => {
       message: 'Failed to update program type!',
     });
   }
-
-
-  // User.findOne({_id: req.userData.userId})
-  //     .then((user) => {
-  //       const student = new Student(user);
-  //       student.appType = req.body.appType;
-  //       student.save()
-  //           .then(
-  //               () => {
-  //                 res.status(201).json({
-  //                   message: 'Program type updated!',
-  //                 });
-  //               },
-  //           )
-  //           .catch(
-  //               () => {
-  //                 res.status(500).json({
-  //                   message: 'Failed to update program type!',
-  //                 });
-  //               },
-  //           );
-  //     })
-  //     .catch(() => {
-  //       res.status(500).json({
-  //         message: 'Failed to set program type!',
-  //       });
-  //     });
 };
 
 module.exports.getAppStatus = (req, res) => {
@@ -114,32 +87,6 @@ module.exports.setAppStatus = async (req, res) => {
       message: 'Failed to update application status.',
     });
   }
-
-  // User.findOne({_id: req.userData.userId})
-  //     .then((user) => {
-  //       const student = new Student(user);
-  //       student.appStatus = req.body.appStatus;
-  //       student.save()
-  //           .then(
-  //               () => {
-  //                 res.status(201).json({
-  //                   message: 'Application status updated!',
-  //                 });
-  //               },
-  //           )
-  //           .catch(
-  //               () => {
-  //                 res.status(500).json({
-  //                   message: 'Failed to update application status!',
-  //                 });
-  //               },
-  //           );
-  //     })
-  //     .catch(() => {
-  //       res.status(500).json({
-  //         message: 'Failed to set application status!',
-  //       });
-  //     });
 };
 
 module.exports.getAppProgress = (req, res) => {
@@ -229,91 +176,3 @@ module.exports.getAppInfo = async (req, res) => {
     console.log(err.stack);
   }
 };
-
-// module.exports.getAppInfo = async (req, res) => {
-//   const appInfo = {
-//     appType: '',
-//     appStatus: '',
-//     appProgress: '',
-//     appSteps: [],
-//   };
-//   try {
-//     const student = await Student.findById(req.userData.userId)
-//         .select('appType appStatus appProgress')
-//         .lean();
-//     const application = await BioApps.findOne({userId: req.userData.userId})
-//         .select(
-//             'studentAcademicInfo.completed ' +
-//           'emergencyContactInfo.completed ' +
-//           'mentorInfo.completed ' +
-//           'internshipInfo.completed ' +
-//           'educationalObjectives.completed ' +
-//           'documents.completed ' +
-//           'signature.completed')
-//         .lean();
-//
-//     if (!student) {
-//       return res.status(404).json({
-//         message: 'Could not find student.',
-//       });
-//     }
-//     if (!application) {
-//       return res.status(404).json({
-//         message: 'Could not find application.',
-//       });
-//     }
-//     appInfo.appType = student.appType;
-//     appInfo.appStatus = student.appStatus;
-//     appInfo.appProgress = student.appProgress;
-//     appInfo.appSteps = [
-//       {'name': 'Student Academic Info', 'completed': application.studentAcademicInfo.completed},
-//       {'name': 'Emergency Contact Info', 'completed': application.emergencyContactInfo.completed},
-//       {'name': 'Mentor Info', 'completed': application.mentorInfo.completed},
-//       {'name': 'Internship Info', 'completed': application.internshipInfo.completed},
-//       {'name': 'Educational Objectives', 'completed': application.educationalObjectives.completed},
-//       {'name': 'Documents', 'completed': application.documents.completed},
-//       {'name': 'Signature', 'completed': application.signature.completed},
-//     ];
-//     res.status(200).json({
-//       appInfo: appInfo,
-//     });
-//   } catch (err) {
-//     err.stack;
-//   }
-//
-//   // Student.findOne({_id: req.userData.userId})
-//   //     .then((student) => {
-//   //       appInfo.appType = student.appType;
-//   //       appInfo.appProgress = student.appProgress;
-//   //       BioApps.findOne({userId: req.userData.userId}, (err, application) => {
-//   //         if (err) {
-//   //           res.status(400).json({
-//   //             message: 'There was an error.',
-//   //           });
-//   //         } else if (application === null) {
-//   //           res.status(404).json({
-//   //             message: 'Application not found!',
-//   //           });
-//   //         } else {
-//   //           appInfo.appSteps = [
-//   //             {'name': 'Student Academic Info', 'completed': application.studentAcademicInfo.completed},
-//   //             {'name': 'Emergency Contact Info', 'completed': application.emergencyContactInfo.completed},
-//   //             {'name': 'Mentor Info', 'completed': application.mentorInfo.completed},
-//   //             {'name': 'Internship Info', 'completed': application.internshipInfo.completed},
-//   //             {'name': 'Educational Objectives', 'completed': application.educationalObjectives.completed},
-//   //             {'name': 'Documents', 'completed': application.documents.completed},
-//   //             {'name': 'Signature', 'completed': application.signature.completed},
-//   //           ];
-//   //
-//   //           res.status(201).json({
-//   //             appInfo: appInfo,
-//   //           });
-//   //         }
-//   //       });
-//   //     })
-//   //     .catch((err) => {
-//   //       res.status(401).json({
-//   //         message: err,
-//   //       });
-//   //     });
-// };
