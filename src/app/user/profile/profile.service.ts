2f1830be02749c5f26a40184add7c39e4a5091c7
EJK0 <ephraimkim1@gmail.com>
2022-05-12 23:53:01 -0400

    removed unused import

--- src/app/user/profile/profile.service.ts
+++ src/app/user/profile/profile.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Profile} from "./profile-model";
-import {Subject} from "rxjs";
 import {map, publishReplay, refCount} from "rxjs/operators";
 
 @Injectable({
